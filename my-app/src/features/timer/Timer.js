b46f28099b1f0c23f2d2d271ecdae45c47244b18
Dwayne Lacey <dwaynelacey.acm@gmail.com>
2024-02-25 18:04:30 -0500

    Added files for application and removed unused code

---
+++ my-app/src/features/timer/Timer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {} from './timerSlice';
+import styles from './Timer.module.css';
+
+export function Timer() {
+  
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
