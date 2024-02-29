86a78383f3f3ad7aba9c7da4932920abf5148960
Dwayne Lacey <dwaynelacey.acm@gmail.com>
2024-02-28 22:06:10 -0500

    Updated dependencies in project to use font awesome, react17, bootstrap

--- my-app/src/features/timer/Timer.js
+++ my-app/src/features/timer/Timer.js
@@ -2,13 +2,37 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {} from './timerSlice';
 import styles from './Timer.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export function Timer() {
   
 
   return (
     <div>
-      
+      <div className="container">
+        <div className="row">
+
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <div className="row">
+              <h2>Break Length</h2>
+            </div>
+            <div className="row">
+              <FontAwesomeIcon icon={[fa, 'arrow-down']}></FontAwesomeIcon>
+            </div>
+          </div>
+          <div className="col-6">
+
+          </div>
+        </div>
+        <div className="row">
+
+        </div>
+        <div className="row">
+
+        </div>
+      </div>
     </div>
   );
 }
