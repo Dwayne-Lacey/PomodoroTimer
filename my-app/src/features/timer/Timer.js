0e87cd5936c5feae094e8d1561014d223d831632
Dwayne Lacey <dwaynelacey.acm@gmail.com>
2024-02-29 21:46:28 -0500

    Added actions to handle incrementing/decrementing session or break length, as well as buttons and icons for user to use.

--- my-app/src/features/timer/Timer.js
+++ my-app/src/features/timer/Timer.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {} from './timerSlice';
+import { 
+  incrementBreak, 
+  decrementBreak,
+  incrementFocus,
+  decrementFocus,
+  selectBreakTime,
+  selectFocusTime
+} from './timerSlice';
 import styles from './Timer.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons'
 
 export function Timer() {
-  
+  const breakTime = useSelector(selectBreakTime);
+  const focusTime = useSelector(selectFocusTime);
+  const dispatch = useDispatch();
+  const adjustTime = (adjustment, type) => {
+
+    // Check current time, max for each timer is 60 minutes
+    switch (adjustment) {
+      case "increment":
+        console.log("incrementing");
+        if ((type === "break" && breakTime <= 59) || (type !== "break" && focusTime <= 59)) {
+          dispatch(type === "break" ? incrementBreak() : incrementFocus());
+        }
+        break;
+      case "decrement":
+        if ((type === "break" && breakTime >= 2) || (type !== "break" && focusTime >= 2)) {
+          dispatch(type === "break" ? decrementBreak() : decrementFocus());
+        }
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <div>
@@ -19,11 +48,28 @@ export function Timer() {
               <h2>Break Length</h2>
             </div>
             <div className="row">
-              <FontAwesomeIcon icon={[fa, 'arrow-down']}></FontAwesomeIcon>
+              <button onClick={() => {adjustTime("decrement", "break")}}>
+                <FontAwesomeIcon icon={faArrowDown}></FontAwesomeIcon>
+              </button>
+              <h3>{breakTime}</h3>
+              <button onClick={() => {adjustTime("increment", "break")}}>
+                <FontAwesomeIcon icon={faArrowUp}></FontAwesomeIcon>
+              </button>
             </div>
           </div>
           <div className="col-6">
-
+            <div className="row">
+              <h2>Session Length</h2>
+            </div>
+            <div className="row">
+              <button onClick={() => {adjustTime("decrement", "focus")}}>
+                <FontAwesomeIcon icon={faArrowDown}></FontAwesomeIcon>
+              </button>
+              <h3>{focusTime}</h3>
+              <button onClick={() => {adjustTime("increment", "focus")}}>
+                <FontAwesomeIcon icon={faArrowUp}></FontAwesomeIcon>
+              </button>
+            </div>
           </div>
         </div>
         <div className="row">
--- my-app/src/features/timer/timerSlice.js
+++ my-app/src/features/timer/timerSlice.js
@@ -1,8 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  value: 0,
-  status: 'idle',
+  breakTime: 5,
+  focusTime: 25,
+  timeElapsed: 0,
+  isFocusTime: true
 };
 
 export const timerSlice = createSlice({
@@ -10,37 +12,28 @@ export const timerSlice = createSlice({
   initialState,
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
+    incrementBreak: (state) => {
+      state.breakTime += 1;
     },
-    decrement: (state) => {
-      state.value -= 1;
+    decrementBreak: (state) => {
+      state.breakTime -= 1;
     },
-    // Use the PayloadAction type to declare the contents of `action.payload`
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
+    incrementFocus: (state) => {
+      state.focusTime += 1;
+    },
+    decrementFocus: (state) => {
+      state.focusTime -= 1;
     },
   }
 });
 
-export const { increment, decrement, incrementByAmount } = timerSlice.actions;
+export const { incrementBreak, decrementBreak, incrementFocus, decrementFocus } = timerSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.timer.value)`
-export const selectCount = (state) => state.timer.value;
+export const selectBreakTime = (state) => state.timer.breakTime;
+export const selectFocusTime = (state) => state.timer.focusTime;
 
-// We can also write thunks by hand, which may contain both sync and async logic.
-// Here's an example of conditionally dispatching actions based on current state.
-export const incrementIfOdd = (amount) => (dispatch, getState) => {
-  const currentValue = selectCount(getState());
-  if (currentValue % 2 === 1) {
-    dispatch(incrementByAmount(amount));
-  }
-};
 
 export default timerSlice.reducer;
